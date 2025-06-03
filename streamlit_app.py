b443aa8b07a74a0e3a26cff26b421f6d70a9f07d
feizicodes <tfeizi@gmx.net>
2025-06-03 16:00:33 +0200

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,22 @@
 
+
 import streamlit as st
 import psycopg
 import pandas as pd
 
-
-
-favourite_mentor = st.selectbox(
-    "Who is your favourite CAB mentor?",
-    ("Emily", "Muayad", "Raul", "Lucas", "Janina"),
-    index=None,
-    placeholder="Select your favourite mentor"
-)
-
-st.write("Your favourite mentor is: ", favourite_mentor)
-
-
+st.write("Secrets keys:", st.secrets.keys())
 def get_api_data():
-  dbconn = st.secrets["DBCONN"]
-  conn = psycopg.connect(dbconn)
-  cur = conn.cursor()
+    dbconn = st.secrets["DBCONN"]
+    conn = psycopg.connect(dbconn)
+    cur = conn.cursor()
 
-  cur.execute('''
-    SELECT * FROM api_data;
-  ''')
-  data = cur.fetchall()
+    cur.execute('SELECT * FROM api_data;')
+    rows = cur.fetchall()
 
-  conn.commit()
-  cur.close()
-  conn.close()
+    cur.close()
+    conn.close()
 
-  return pd.DataFrame(data, columns=["date", "open", "close"])
+    return pd.DataFrame(rows, columns=["date", "open", "close"])
 
 api_data = get_api_data()
 st.dataframe(api_data)

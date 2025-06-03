2f5805afc304282da49f80fbda57c98b12c69e4e
feizicodes <tfeizi@gmx.net>
2025-06-03 16:10:59 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,16 @@
-
-
 import streamlit as st
 import psycopg
 import pandas as pd
 
-st.write("Secrets keys:", st.secrets.keys())
 def get_api_data():
     dbconn = st.secrets["DBCONN"]
     conn = psycopg.connect(dbconn)
     cur = conn.cursor()
-
-    cur.execute('SELECT * FROM api_data;')
+    cur.execute("SELECT * FROM api_data;")
     rows = cur.fetchall()
-
     cur.close()
     conn.close()
-
     return pd.DataFrame(rows, columns=["date", "open", "close"])
 
-api_data = get_api_data()
-st.dataframe(api_data)
+st.dataframe(get_api_data())
+
